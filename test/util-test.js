fae7b0b1bc13396e67d94705d984c3c3c7c3aa4c
Rasmus Borgsmidt <rasmus@borgsmidt.dk>
2018-11-09 14:20:12 +0100

    code to resolve validity parameters

---
+++ test/util-test.js
@@ -0,0 +1,76 @@
+"use strict";
+
+var chai = require('chai');
+chai.use(require('chai-as-promised'));
+var expect = chai.expect;
+var util = require('../lib/util');
+
+describe('Util', function() {
+    describe('resolveValidityParameters', function() {
+        var invalid = 'not a valid date string';
+        var now = new Date().toISOString();
+        var later = new Date(3000, 0, 1).toISOString();
+        var beginning = new Date('0001-01-01').toISOString();
+        var end = new Date('9999-12-31').toISOString();
+
+        it('should accept context with validOn parameter', function(done) {
+            expect(util.resolveValidityParameters({ validOn: now }))
+                .to.eventually.deep.equal({ validOn: now })
+                .notify(done);
+        });
+
+        it('should accept context with just validFrom parameter', function(done) {
+            expect(util.resolveValidityParameters({ validFrom: now }))
+                .to.eventually.deep.equal({ validFrom: now, validTo: end })
+                .notify(done);
+        });
+
+        it('should accept context with validOn parameter', function(done) {
+            expect(util.resolveValidityParameters({ validTo: now }))
+                .to.eventually.deep.equal({ validFrom: beginning, validTo: now })
+                .notify(done);
+        });
+
+        it('should accept context with validFrom and validTo parameters', function(done) {
+            expect(util.resolveValidityParameters({ validFrom: now, validTo: later }))
+                .to.eventually.deep.equal({ validFrom: now, validTo: later })
+                .notify(done);
+        });
+
+        it('should reject context with invalid validOn parameter', function(done) {
+            expect(util.resolveValidityParameters({ validOn: invalid }))
+                .to.eventually.be.rejectedWith('invalid validOn parameter in context')
+                .notify(done);
+        });
+
+        it('should reject context with invalid validFrom parameter', function(done) {
+            expect(util.resolveValidityParameters({ validFrom: invalid, validTo: later }))
+                .to.eventually.be.rejectedWith('invalid validFrom parameter in context')
+                .notify(done);
+        });
+
+        it('should reject context with invalid validTo parameter', function(done) {
+            expect(util.resolveValidityParameters({ validFrom: now, validTo: invalid }))
+                .to.eventually.be.rejectedWith('invalid validTo parameter in context')
+                .notify(done);
+        });
+
+        it('should reject context with validOn and validFrom parameters', function(done) {
+            expect(util.resolveValidityParameters({ validOn: now, validFrom: later }))
+                .to.eventually.be.rejectedWith('both validOn and validFrom/validTo specified')
+                .notify(done);
+        });
+
+        it('should reject context with validOn and validTo parameters', function(done) {
+            expect(util.resolveValidityParameters({ validOn: now, validTo: later }))
+                .to.eventually.be.rejectedWith('both validOn and validFrom/validTo specified')
+                .notify(done);
+        });
+
+        it('should reject context with validFrom > validTo', function(done) {
+            expect(util.resolveValidityParameters({ validFrom: later, validTo: now }))
+                .to.eventually.be.rejectedWith('validFrom > validTo in context')
+                .notify(done);
+        });
+    });
+});
